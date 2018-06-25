f06d39a334512254024ea46e4572c329eae7b81d
Gary White <g.z.white@mac.com>
2018-06-25 22:26:53 +0100

    Some prints.

--- smfml/gaussian_distribution /generate_sample_of_random_gaussian.py
+++ smfml/gaussian_distribution /generate_sample_of_random_gaussian.py
@@ -1,13 +1,20 @@
 # generate a sample of random gaussian
 from numpy.random import seed
 from numpy.random import randn
+from numpy import mean
 from matplotlib import pyplot as plt
 
 seed(1)
 
 # generate univariate observations
 data = 5 * randn(10000) + 50
+print(type(data))
+print(data.shape)
+print(data[0])
+print(data[-1])
+
+print('mean: {0}'.format(mean(data)))
 
 # histogram of generated data
-plt.hist(data)
+plt.hist(data, bins=100)
 plt.show()
